428432743d791a6ae3b0e23508db52891cd28e3e
Tim Anderson <timand@mail.med.upenn.edu>
2015-01-06 16:41:30 -0500

    ido is annoying

--- init.el
+++ init.el
@@ -31,6 +31,15 @@
   (interactive)
   (dired "/timand@webdev2.med.upenn.edu:~"))
 
+;; hopefully a better ido
+;;(require 'flx-ido)
+;;(ido-mode 1)
+;;(ido-everywhere 1)
+;;(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+;;(setq ido-enable-flex-matching t)
+;;(setq ido-use-faces nil)
+
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
